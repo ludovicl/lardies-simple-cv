ed1db7ff8a06b5dedb77a843588b631b0ef14559
ludovicl <ludoviclrds@gmail.com>
2016-01-02 19:38:40 +0100

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+/**
+ * Created by ludovicl on 27/12/2015.
+ */
+
+$(document).on('click', '.panel div.clickable', function () {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+
+$(document).ready(function () {
+    $('.panel div.clickable').click();
+});
+
+$("#pop").on("click", function() {
+    $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
+
+$('.modal-link').click(function (e) {
+    $('#myModal img').attr('src', $(this).attr('data-img-url'));
+});
+
