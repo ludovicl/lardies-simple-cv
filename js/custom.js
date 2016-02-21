d333355009c787bb76b2f333ce3bd604cb772a4f
ludovicl <ludoviclrds@gmail.com>
2016-02-21 14:19:54 +0100

    delete useless files & add active nav bar

--- js/custom.js
+++ js/custom.js
@@ -25,6 +25,14 @@ $(document).ready(function () {
     $('.panel div.clickable').click();
 });
 
+$(document).ready(function () {
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).parent().addClass('active');
+});
+
 
 function get_windows_size(){
     var size = 'md';
